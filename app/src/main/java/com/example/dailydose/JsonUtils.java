5722b19b59807c552251335f87fb3d51f79ff75e
Hunter Fitzgerald <hlfitz98@cs.washington.edu>
2021-05-04 20:31:29 -0700

    Added backend functionality to get all tags, to get a particular entry by id, to delete a particular entry by id, to add just one entry, and by proxy to edit an entry (get by id and then add it and it will replace the old one)

--- app/src/main/java/com/example/dailydose/JsonUtils.java
+++ app/src/main/java/com/example/dailydose/JsonUtils.java
@@ -24,7 +24,7 @@ import java.lang.reflect.Type;
 import java.util.*;
 
 public class JsonUtils {
-    private static boolean emptyFile = true;
+
     /**
      * Writes List of Entries to "Entries.json"
      * USE: Write all entries to "Entries.json" MAKE SURE LIST
@@ -46,10 +46,8 @@ public class JsonUtils {
         String jsonStr;
         if (entryArr.isEmpty()) {
             jsonStr = "";
-            emptyFile = true;
         } else {
             jsonStr = entryArr.toJSONString();
-            emptyFile = false;
         }
         try (FileWriter writer = new FileWriter("Entries.json")) {
             writer.write(jsonStr);
@@ -93,6 +91,75 @@ public class JsonUtils {
     }
 
     /**
+     * Get all of the tags that are currently in the data file
+     * @return A Set of strings representing all tags in the data
+     */
+    public static Set<String> getAllTags() {
+        Set<String> result = new HashSet<>();
+        List<Entry> entries = getEntries();
+        for (Entry e: entries) {
+            for (String tag: e.getTags()) {
+                result.add(tag);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Delete entry with the given id
+     * @param id the id of the entry to delete
+     * @return True if deleted, false if that id is not in the file
+     */
+    public static boolean delete(int id) {
+        List<Entry> entries = getEntries();
+        boolean found = false;
+        int position = -1;
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getId() == id) {
+                found = true;
+                position = i;
+            }
+        }
+
+        if (found) {
+            entries.remove(position);
+        }
+        writeEntries(entries);
+        return found;
+    }
+
+    /**
+     * Get a particular entry by id
+     * USE: to EDIT an ENTRY call GET(id) then change what needs changed,
+     * and finally call WRITEENTRY(entry)
+     * @param id the entry to retrieve
+     * @return the entry the id represents, or null if the
+     * id does not exist
+     */
+    public static Entry get(int id) {
+        List<Entry> entries = getEntries();
+        for (int i = 0; i < entries.size(); i++) {
+            if (entries.get(i).getId() == id) {
+                return entries.get(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Add an entry to the database, returns whether it is replacing an existing entry
+     * @param entry entry to write
+     * @return true if replacing existing entry by same id, false if new entry
+     */
+    public static boolean writeEntry(Entry entry) {
+        boolean result = delete(entry.getId());
+        List<Entry> entries = getEntries();
+        entries.add(entry);
+        writeEntries(entries);
+        return result;
+    }
+
+    /**
      * Parses JSON Object back into an Entry
      * @param entryObject JSON Object from file
      * @return Entry that entryObject represented
@@ -102,5 +169,4 @@ public class JsonUtils {
         return new Entry((String) entryObject.get("content"), (double) entryObject.get("rating"),
                 l.intValue() , (List<String>) entryObject.get("tags"));
     }
-
 }
--- app/src/test/java/com/example/dailydose/JsonFileTests.java
+++ app/src/test/java/com/example/dailydose/JsonFileTests.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class JsonFileTests {
@@ -36,5 +40,144 @@ public class JsonFileTests {
 
         List<Entry> result = JsonUtils.getEntries();
         assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void getAllTagsTest() {
+        List<String> tags = new ArrayList<>();
+        tags.add("Store");
+        tags.add("Shopping");
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        Set<String> result = JsonUtils.getAllTags();
+        assertArrayEquals(tags.toArray(), result.toArray());
+    }
+
+    @Test
+    public void getAllTagsEmptyTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        Set<String> result = JsonUtils.getAllTags();
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void deleteTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        assertTrue(JsonUtils.delete(1));
+        List<Entry> result = JsonUtils.getEntries();
+        assertEquals(result.size(), 1);
+        assertEquals(result.get(0), entry2);
+    }
+
+    @Test
+    public void deleteInvalidIdTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        assertFalse(JsonUtils.delete(34));
+    }
+
+    @Test
+    public void getTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        assertEquals(entry, JsonUtils.get(1));
+    }
+
+    @Test
+    public void getInvalidIdTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+
+        assertEquals(null, JsonUtils.get(122));
+    }
+
+    @Test
+    public void addExistingIdTest() {
+        List<String> tags = new ArrayList<>();
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+        boolean existingID = JsonUtils.writeEntry(entry2);
+        List<Entry> result = JsonUtils.getEntries();
+        assertEquals(2, result.size());
+        assertTrue(result.contains(new Entry("Video Games", 8, 2, tags)));
+        assertTrue(result.contains(new Entry("Went to the store today", 9.5, 1, tags)));
+        assertTrue(existingID);
+    }
+
+    @Test
+    public void addNewIdTest() {
+        List<String> tags = new ArrayList<>();
+        tags.add("fun");
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        Entry entry2 = new Entry("Video Games", 8, 2, tags);
+        Entry entry3 = new Entry("Going Swimming", 5, 3, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        entries.add(entry2);
+        JsonUtils.writeEntries(entries);
+        boolean existingID = JsonUtils.writeEntry(entry3);
+        List<Entry> result = JsonUtils.getEntries();
+        assertEquals(3, result.size());
+        assertTrue(result.contains(new Entry("Video Games", 8, 2, tags)));
+        assertTrue(result.contains(new Entry("Went to the store today", 9.5, 1, tags)));
+        assertTrue(result.contains(new Entry("Going Swimming", 5, 3, tags)));
+        assertFalse(existingID);
+    }
+
+    @Test
+    public void editTest() {
+        List<String> tags = new ArrayList<>();
+        tags.add("fun");
+        Entry entry = new Entry("Went to the store today", 9.5, 1, tags);
+        List<Entry> entries = new ArrayList<>();
+        entries.add(entry);
+        JsonUtils.writeEntries(entries);
+
+        Entry entry2 = JsonUtils.get(1);
+        entry2.setRating(10);
+        JsonUtils.writeEntry(entry2);
+        List<Entry> result = JsonUtils.getEntries();
+        assertEquals(result.size(), 1);
+        assertTrue(result.contains(new Entry("Went to the store today", 10, 1, tags)));
     }
 }
