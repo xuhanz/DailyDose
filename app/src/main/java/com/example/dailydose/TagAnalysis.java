d34bba025a05f29a058a18478024b83c90ab6dda
mjg1124 <mjg1124@uw.edu>
2021-05-06 13:56:27 -0700

    Added basic tag analysis

---
+++ app/src/main/java/com/example/dailydose/TagAnalysis.java
@@ -0,0 +1,26 @@
+package com.example.dailydose;
+import java.util.*;
+
+public class TagAnalysis {
+	/**
+	 * Calculate the average rating of entries with the given tag
+	 * @param tag the tag to calculate the average rating of
+	 * @param filename the file to pull entries from
+	 * @return Double of average rating; returns 0 if there are no entries or none with tag
+	 */
+	public double getAvgRating(String tag, String filename) {
+
+		double rating = 0;
+		int entriesWithTag = 0;
+		List<Entry> entries = JsonUtils.getEntries(filename);
+		// if tags or entries do not exist, immediately exit
+		if (entries == null || !JsonUtils.getAllTags(filename).contains(tag)) { return 0; }
+		for (Entry entry : entries) {  // iterate through entries
+			if (entry.getTags().contains(tag)) {  // if entry has tag, add rating
+				rating += entry.getRating();
+				entriesWithTag++;
+			}
+		}
+		return rating / entriesWithTag;
+	}
+}
