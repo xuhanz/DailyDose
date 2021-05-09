c19ec98ede0c3404e81ff5123a672ccefa4d00d4
mjg1124 <mjg1124@uw.edu>
2021-05-09 11:50:36 -0700

    Refactored TagAnalysis, rewrote tests

--- app/src/main/java/com/example/dailydose/TagAnalysis.java
+++ app/src/main/java/com/example/dailydose/TagAnalysis.java
@@ -1,28 +1,40 @@
 package com.example.dailydose;
 import android.content.Context;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class TagAnalysis {
 	/**
-	 * Calculate the average rating of entries with the given tag
-	 * @param tag the tag to calculate the average rating of
-	 * @param filename the file to pull entries from
-	 * @return Double of average rating; returns 0 if there are no entries or none with tag
+	 * Filters a list of entries for those with the given tag
+	 * @param entries the entries to filter by tags
+	 * @param tag the tag to filter by
+	 * @return List of entries with tag; returns empty list if entries arg null or empty
 	 */
-	public static double getAvgRating(String tag, String filename, Context context) {
-
-		double rating = 0;
-		int entriesWithTag = 0;
-		List<Entry> entries = JsonUtils.getEntries(filename, context);
-		// if tags or entries do not exist, immediately exit
-		if (entries == null || !JsonUtils.getAllTags(filename, context).contains(tag)) { return 0; }
-		for (Entry entry : entries) {  // iterate through entries
-			if (entry.getTags().contains(tag)) {  // if entry has tag, add rating
-				rating += entry.getRating();
-				entriesWithTag++;
+	public static List<Entry> filterEntriesByTag(List<Entry> entries, String tag) {
+		List<Entry> filteredEntries = new ArrayList<>();
+		if (entries == null || entries.size() == 0) { return filteredEntries; }
+		for (Entry entry : entries) {
+			if (entry.getTags().contains(tag)) {
+				filteredEntries.add(entry);
 			}
 		}
-		return rating / entriesWithTag;
+		return filteredEntries;
+	}
+
+	/**
+	 * Calculate the average rating of the given entries
+	 * @param entries the entries to get the average rating of
+	 * @return Double of average rating; returns 0 if there are no entries
+	 */
+	public static double getAvgRating(List<Entry> entries) {
+
+		double rating = 0;
+		// if tags or entries do not exist, immediately exit
+		if (entries == null || entries.size() == 0) { return 0; }
+		for (Entry entry : entries) {  // iterate through entries
+			rating += entry.getRating();
+		}
+		return rating / entries.size();
 	}
 }
