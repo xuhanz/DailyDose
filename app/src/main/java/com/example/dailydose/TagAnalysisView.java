ceb55797b894d05079c97292ea019f22d4f14473
Ester Yang <yang2022@cs.washington.edu>
2021-05-23 15:07:41 -0700

    Resolved the issue of Calendar, now it shows the date the user previously picked. Fixed the color of the action bar of the App.

--- app/src/main/java/com/example/dailydose/TagAnalysisView.java
+++ app/src/main/java/com/example/dailydose/TagAnalysisView.java
@@ -227,11 +227,18 @@ public class TagAnalysisView extends AppCompatActivity{
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
+            int year;
+            int month;
+            int day;
+            if(startOrEnd){
+                year = startDate.getYear()+1900;
+                month = startDate.getMonth();
+                day = startDate.getDate();
+            } else {
+                year = endDate.getYear()+1900;
+                month = endDate.getMonth();
+                day = endDate.getDate();
+            }
             // Create a new instance of DatePickerDialog and return it
             return new DatePickerDialog(getActivity(), this, year, month, day);
         }
